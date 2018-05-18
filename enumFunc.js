270df7e20ecc70466e98e22d036ed536a326a62f
Raul Torres <rto07@users.noreply.github.com>
2018-05-18 16:29:31 -0500

    Update enumFunc.js

--- enumFunc.js
+++ enumFunc.js
@@ -1,9 +1,13 @@
+//New Code
 var numbers = [1,2,3,4,5,6,7,8,9,2,3,4,5,6,1,2,3,4,5,6,7,8,8,4,3,2];
 var sum = numbers
-.filter(item => item > 4)
-//I don't understand how the < and > signs work in this situation. 
-//I would have thought item > 4 meant item is greater than 4. 
-//Thus, it would filter anything greater than 4. 
-//Why does it read as item is less than 4 in this case?
+.filter(item => item >= 4)
 .reduce((sum, item) => sum + item);
 console.log(sum);
+
+//Old Code
+/*var numbers = [1,2,3,4,5,6,7,8,9,2,3,4,5,6,1,2,3,4,5,6,7,8,8,4,3,2];
+var sum = numbers
+.filter(item => item > 4) //want to keep the 4's, you had to add >=
+.reduce((sum, item) => sum + item);
+console.log(sum);*/
